248a76511a9acfe564904aeffa2c6445946df340
Viktoryia Tarasevich <viktoryia.tarasevich@itechart-group.com>
2021-02-28 09:56:32 +0300

    Added Input Manager

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : Singleton<InputManager>
+{
+    //Величина смещения. -1.0 = максимально влево, 1 = максимально вправо
+    private float _sidewaysMotion = 0.0f;
+
+    public float sidewaysMotion
+    {
+        get
+        {
+            return _sidewaysMotion;
+        }
+    }
+
+    void Update()
+    {
+        Vector3 accel = Input.acceleration;
+        //Сохраняем величину отклонения в каждом кадре
+        _sidewaysMotion = accel.x;
+    }
+}
---
+++ Assets/Scripts/Swinging.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swinging : MonoBehaviour
+{
+    public float swingSensitivity = 100.0f;
+
+
+    void FixedUpdate()
+    {
+        if(GetComponent<Rigidbody2D>() == null)
+        {
+            Destroy(this);
+            return;
+        }
+
+        //Получить величну из наклона
+        float swing = InputManager.Instance.sidewaysMotion;
+
+        //Вычислить прилагаемую силу
+        Vector2 force = new Vector2(swing * swingSensitivity, 0);
+
+        //Приложить силу
+        GetComponent<Rigidbody2D>().AddForce(force);
+    }
+}
